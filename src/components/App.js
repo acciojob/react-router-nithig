cf4e1ab909bdcf9a5c58f016a73df7caa55c2676
nithig <gnithishdeveloper@gmail.com>
2025-07-28 17:06:52 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+const Home = () => {
+  return <h1>Welcome to my website!</h1>;
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a sample React Router program.</p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
